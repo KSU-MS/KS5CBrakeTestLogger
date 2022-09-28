ed01c3071a5d8ce0e98c6f3a59a5dba7b560e660
mathbrook <32876429+mathbrook@users.noreply.github.com>
2022-09-27 20:28:15 -0400

    yeee

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
-#include <FreqMeasureMulti.h>
 #include <Adafruit_ADS1X15.h>
-#include "SdFat.h"
-
+#include <SdFat.h>
+#include <SimpleKalmanFilter.h>
+SimpleKalmanFilter shonkFL(1,1,0.01); //front left shonk
+SimpleKalmanFilter shonkFR(1,1,0.01); //front right shonk
+SimpleKalmanFilter shonkRL(1,1,0.01); //rear left shonk
+SimpleKalmanFilter shonkRR(1,1,0.01); //rear right shonk
 #ifndef SDCARD_SS_PIN
 const uint8_t SD_CS_PIN = SS;
 #else  // SDCARD_SS_PIN
@@ -11,37 +14,15 @@ const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
 #endif  // SDCARD_SS_PIN
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 //Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
-
-#define RPM_TIMEOUT 100 // timeout on RPM to settle to zero 
-// Measure 3 frequencies at the same time! :-)
-FreqMeasureMulti freq1;
-FreqMeasureMulti freq2;
-FreqMeasureMulti freq3;
-double current_rpm = 0;
-
-float prev_rpm;
-unsigned long current_rpm_change_time;
-int sum = 0;
-int count = 0;
-unsigned long time1;
-double current_rpm2 = 0;
-
-float prev_rpm2;
-unsigned long current_rpm_change_time2;
-int sum2 = 0;
-int count2 = 0;
-unsigned long time2;
-double current_rpm3 = 0;
-
-float prev_rpm3;
-unsigned long current_rpm_change_time3;
-int sum3 = 0;
-int count3 = 0;
-unsigned long time3;
 //adc variables
 //space out bt using pins 0 and 3 to avoid short on board
-int16_t adc0, adc3;
-float volts0, volts3;
+int16_t adc0,adc1,adc2,adc3;
+//0-3 is purple, blue,yellow,orange
+//0-RR
+//1-FL
+//2-FR
+//3-RL
+float volts0,volts1,volts2, volts3;
 //string for writing to SD
 String dataLine;
 unsigned long log_rate=0;
@@ -72,7 +53,7 @@ typedef FsFile file_t;
 #else  // SD_FAT_TYPE
 #error Invalid SD_FAT_TYPE
 #endif  // SD_FAT_TYPE
-#define FILE_BASE_NAME "BrakeTestLog"
+#define FILE_BASE_NAME "ShonkPotLog"
 file_t myFile;
 const uint8_t BASE_NAME_SIZE=sizeof(FILE_BASE_NAME)-1;
 char fileName[] = FILE_BASE_NAME "00.txt";
@@ -83,10 +64,9 @@ void setup() {
   delay(10);
 
   // picking pin 5
-  freq2.begin(6);
-  freq1.begin(5);
-  freq3.begin(9);
+
   if (!sd.begin(SD_CONFIG)) {
+    digitalWrite(ledPin,HIGH);
     sd.initErrorHalt(&Serial);
   }
   while (sd.exists(fileName)) {
@@ -96,7 +76,7 @@ void setup() {
       fileName[BASE_NAME_SIZE + 1] = '0';
       fileName[BASE_NAME_SIZE]++;
     } else {
-      //Serial.println(F("Can't create file name"));
+      Serial.println(F("Can't create file name"));
       return;
     }
   }
@@ -105,16 +85,13 @@ void setup() {
   }
   //myFile.open(fileName);
   myFile.println(fileName);
-  myFile.println("Time,LeftWheel,RightWheel,OutputShaft,Brake1,Brake2");
+  myFile.println("Time,FrontLeft,FrontRight,RearLeft,RearRight");
    //Serial.begin(57600);
   //while (!Serial) ; // wait for Arduino Serial Monitor
   delay(10);
- // Serial.println("Getting single-ended readings from AIN0..3");
-  //Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-  //Serial.println("FreqMeasureMulti Begin");
-  ads.setGain(GAIN_TWOTHIRDS);
+  ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV
   if (!ads.begin()) {
-    //Serial.println("Failed to initialize ADS.");
+    Serial.println("Failed to initialize ADS.");
     while (1);
   }
 }
@@ -122,111 +99,28 @@ void setup() {
 void loop() {
   digitalWrite(ledPin, LOW);
   adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
   adc3 = ads.readADC_SingleEnded(3);
+  int16_t estimated_FL=shonkFL.updateEstimate(adc1);
+  int16_t estimated_FR=shonkFR.updateEstimate(adc2);
+  int16_t estimated_RL=shonkRL.updateEstimate(adc3);
+  int16_t estimated_RR=shonkRR.updateEstimate(adc0);
 
-  volts0 = ads.computeVolts(adc0);
-  volts3 = ads.computeVolts(adc3);
-
-  unsigned long test = millis();
-  if((test - current_rpm_change_time) > RPM_TIMEOUT) {
-    // rpm.data = 0;
-    current_rpm = 0;
-
-  }
-  if (freq1.available()) {
-    // average several reading together
-    sum = sum + freq1.read();
-    count = count + 1;
-    current_rpm_change_time = millis();
-    if (count > 1) {
-        float testRpm = freq1.countToFrequency(sum / count)*60 /18;
-        current_rpm = (double)testRpm;
-
-        /*if ( testRpm - prev_rpm < 1)
-        {
-          current_rpm = (double)testRpm;
-          prev_rpm = testRpm;
-        }*/
-        // Serial.printf("Current RPM: %f \n", testRpm);
-      sum = 0;
-      count = 0;
-      //prev_rpm = testRpm;
-    }
-  }
-  unsigned long test2 = millis();
-  if((test2 - current_rpm_change_time2) > RPM_TIMEOUT) {
-    // rpm.data = 0;
-    current_rpm2 = 0;
-
-  }
-  if (freq2.available()) {
-    // average several reading together
-    sum2 = sum2 + freq2.read();
-    count2 = count2 + 1;
-    current_rpm_change_time2 = millis();
-    if (count2 > 1) {
-        float testRpm2 = freq2.countToFrequency(sum2 / count2) *60/18;
-        current_rpm2 = (double)testRpm2;
-        /*if ( testRpm2 - prev_rpm2 < 1)
-        {
-          current_rpm2 = (double)testRpm2;
-          prev_rpm2 = testRpm2;
-        }*/
-        // Serial.printf("Current RPM2: %f \n", testRpm);
-      sum2 = 0;
-      count2 = 0;
-      prev_rpm2 = testRpm2;
-    }
-  }
-   unsigned long test3 = millis();
-  if((test3 - current_rpm_change_time3) > RPM_TIMEOUT) {
-    // rpm.data = 0;
-    current_rpm3 = 0;
-
-  }
-  if (freq3.available()) {
-    // average several reading together
-    sum3 = sum3 + freq3.read();
-    count3 = count3 + 1;
-    current_rpm_change_time3 = millis();
-    if (count3 > 1) {
-        float testRpm3 = freq3.countToFrequency(sum3 / count3) *60/20;
-        current_rpm3 = (double)testRpm3;
-        /*if ( testRpm2 - prev_rpm2 < 1)
-        {
-          current_rpm2 = (double)testRpm2;
-          prev_rpm2 = testRpm2;
-        }*/
-        // Serial.printf("Current RPM2: %f \n", testRpm);
-      sum3 = 0;
-      count3 = 0;
-      prev_rpm3 = testRpm3;
-    }
-  }
-  /*if(time2<millis())
-  {time2+=200;
-  if (current_rpm2>-1){
-    Serial.print("left wheel: ");
-    Serial.println(current_rpm2);
-  }
-}
-  if(time1<millis()){
-    time1+=200;
-  if (current_rpm>-1){
-    Serial.print("right wheel:");
-    Serial.println(current_rpm);
-  }
-}*/
+ //0-RR
+//1-FL
+//2-FR
+//3-RL
 float now = millis();
 float timestamp = now/1000;
 char buffer[80];
   int n;
-  n=sprintf(buffer,"%f,%f,%f,%f,%f,%f\n",timestamp,current_rpm2,current_rpm,current_rpm3,volts0,volts3);
+  n=sprintf(buffer,"%f,%d,%d,%d,%d\n",timestamp,estimated_FL,estimated_FR,estimated_RL,estimated_RR);
 if(millis()-log_rate>=100){
   log_rate=millis();
   digitalWrite(ledPin, HIGH);
   
-  
+  //  myFile.println("Time,FrontLeft,FrontRight,RearLeft,RearRight");
   //n=sprintf(buffer,"Time\t%f\tLeft wheel\t%f\tRight wheel\t%f\tBrake 1\t%fBrake 2\t%f\n",timestamp,current_rpm2,current_rpm,volts0,volts3);
   myFile.open(fileName,FILE_WRITE);
   myFile.write(buffer);
